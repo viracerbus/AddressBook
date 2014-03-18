2adc618c6d014c9a6eaeb085057c7512f60d4e8b
Dustin Luhmann <dluhmann@gustavus.edu>
2014-03-17 19:31:29 -0500

    Initial Commit

---
+++ AddressBook/src/com/gac/edu/mcs270/hvidsten/client/GreetingService.java
@@ -0,0 +1,12 @@
+package com.gac.edu.mcs270.hvidsten.client;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+/**
+ * The client-side stub for the RPC service.
+ */
+@RemoteServiceRelativePath("greet")
+public interface GreetingService extends RemoteService {
+	String greetServer(String name) throws IllegalArgumentException;
+}
